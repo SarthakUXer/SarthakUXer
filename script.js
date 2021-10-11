e03f1a158c35941b74b73c3f599a0606f67af2e2
SarthakUXer <92269292+SarthakUXer@users.noreply.github.com>
2021-10-11 09:11:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,3 @@ skrollr.init({
   forceHeight: false
 });
 
-$(function() {
-  $('a[href*=#]').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-  });
-});
